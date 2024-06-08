6b11c901df051ed7d3b66cfc6cdc977ac335a66e
e1turin <ivturnik2003@gmail.com>
2024-06-09 02:30:31 +0300

    fix docs

---
+++ res/setup.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+# Use copy-paste if you can't grant execution permissions on file!
+
+# for ubuntu 22.04, kernel 5.15
+sudo apt update
+sudo apt install -y \
+        make \
+        build-essential \
+        linux-generic 
+
